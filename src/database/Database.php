55ac7ce31befe9b588bc3c3da18aafe2580b5256
Pierre Guillaume <papswell@gmail.com>
2019-04-01 15:42:04 +0200

    adding Database object

--- src/database/Database.php
+++ src/database/Database.php
@@ -1,5 +1,50 @@
 <?php 
 
-$database = new PDO('mysql:host=localhost;dbname=relais-du-phare;charset=utf8', 'root', '');
+class Database
+{
+  // Constante de classe
+  const CHARSET = 'utf8';
 
-$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // Attributs
+  private $host;
+  private $name;
+  private $user;
+  private $password;
+
+  private $pdo;
+
+  // Méthodes
+  public function __construct($dbname, $username, $pwd, $host = 'localhost')
+  {
+    $this->name = $dbname;
+    $this->user = $username;
+    $this->password = $pwd;
+    $this->host = $host;
+
+    // Connect to database
+    $this->pdo = new PDO(
+      "mysql:host=$this->host;dbname=$this->name;charset=" . self::CHARSET,
+      $this->user,
+      $this->password
+    );
+
+    // Set error mode to exception
+    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+
+  public function getPDO()
+  {
+    return $this->pdo;
+  }
+
+  public function query($sql)
+  {
+    return $this->pdo->query($sql);
+  }
+}
+
+$database = new Database('relais-du-phare', 'root', '', 'localhost:8889');
+
+
+
+
